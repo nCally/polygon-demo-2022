e04c0ce6bef2db252b4c839caad22cf133eee3c9
Cally Nnamani <cartrf@live.com>
2022-11-10 09:35:40 +0100

    ..

--- src/wallet/GenerateAddress.jsx
+++ src/wallet/GenerateAddress.jsx
@@ -1,50 +1,47 @@
-import React, {useState} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { generateSwitchWalletAddress } from '../methods/app';
-import { Button, Input, Modal } from 'antd';
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { generateSwitchWalletAddress } from "../methods/app";
+import { Button, Input, Modal } from "antd";
 
 function GenerateAddress() {
-	const dispatch = useDispatch();
-	const [open, setOpen] = useState(false);
-	const [email, setEmail] = useState('');
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
 
-	const { profile } = useSelector((store) => store.app);
+  const generate = () => {
+    setOpen(false);
+    dispatch(generateSwitchWalletAddress(email));
+  };
 
-	const generate = () => {
-		setOpen(false)
-		dispatch(generateSwitchWalletAddress(email));
-	};
+  return (
+    <div>
+      <p>Wallet Address from SwitchWallet</p>
+      <br />
+      <Button onClick={() => setOpen(true)}>Generate Address</Button>
+      <Modal
+        footer={null}
+        width={320}
+        open={open}
+        onCancel={() => setOpen(false)}
+        title="Generate address"
+      >
+        <div>
+          <p>User Email?</p>
+          <Input
+            addonAfter="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <br />
 
-	return (
-		<div>
-			<p>Wallet Address from SwitchWallet</p>
-			<br />
-				<Button onClick={() =>setOpen(true) }>Generate Address</Button>
-			<Modal
-				footer={null}
-				width={320}
-				open={open}
-				onCancel={() => setOpen(false)}
-				title="Generate address"
-			>
-				<div>
-					<p>User Email?</p>
-					<Input addonAfter="Email" onChange={(e) => setEmail(e.target.value)}/>
-				</div>
-				<br />
+        <br />
 
-				
-
-				<br />
-
-
-				<Button block type="primary" onClick={() => generate()}>
-					Submit
-				</Button>
-			</Modal>
-		</div>
-	);
+        <Button block type="primary" onClick={() => generate()}>
+          Submit
+        </Button>
+      </Modal>
+    </div>
+  );
 }
 
 export default GenerateAddress;
