d00acc47163caaa236d76b1f34bd4bd95050ef36
Cally Nnamani <cartrf@live.com>
2022-11-10 09:47:50 +0100

    replaced toast with antd message

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
-import App from './App';
-import appState from './methods/store';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import appState from "./methods/store";
 
-import './index.css';
+import "./index.css";
 
 appState().then((store) => {
-	const root = ReactDOM.createRoot(document.getElementById('root'));
-	root.render(
-		<Provider store={store}>
-			<App />
-			<ToastContainer
-				position="top-right"
-				autoClose={2000}
-				hideProgressBar
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				draggable
-			/>
-		</Provider>
-	);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
